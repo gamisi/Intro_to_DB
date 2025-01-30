a248dadb87ac248b4f7a16a4017b9bd6cbca61d1
gamisi <gamisi.ga@gmail.com>
2025-01-30 13:41:49 +0300

    added implementation of the database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,28 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-
+  
     host="localhost",
     user="root",
     password="Qazwsx1234?",
     database=""
     
 )
+
+mycursor = mydb.cursor()
+
+#CREATE DATABASE IF NOT EXIST
+mycursor.execute("CREATE DATABASE IF NOT EXISTS your_database_")
+print('Database created successfully')
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Authors (
+  author_id INT AUTO_INCREMENT PRIMARY KEY,
+  author_name VARCHAR(255) NOT NULL
+  )
+""")
+
+print("Authors table created successfully")
+
+mycursor.close()
+mydb.close()
