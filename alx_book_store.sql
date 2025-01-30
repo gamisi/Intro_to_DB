a8ba59af6720be4f0d536fe3c07a3f9a49d673a4
gamisi <gamisi.ga@gmail.com>
2025-01-30 14:13:03 +0300

    added implementation of the database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS Orders(
 
 );
 
-CREATE TABLE IF NOT EXISTS Orders(
+CREATE TABLE IF NOT EXISTS Order_Details(
 
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
